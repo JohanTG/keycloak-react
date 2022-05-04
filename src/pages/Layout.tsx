8409a5b5d3620022a44b48ea981c956257da8a18
Max Shipko <m.shchipko@gmail.com>
2022-05-04 17:04:59 +0300

    Fix `Using UNSAFE_componentWillMount in strict mode is not recommended and may indicate bugs in your code.`

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { Layout as AntdLayout } from 'antd';
 import styles from './Layout.module.scss';
 import { Outlet } from "react-router-dom";
-import {Helmet} from "react-helmet";
+import {HelmetProvider, Helmet} from "react-helmet-async";
 
 const { Header, Content } = AntdLayout;
 
 function Layout() {
   return (
     <AntdLayout style={{minHeight:"100vh"}}>
-      <Helmet>
-        <title>Rick and Morty: List</title>
-        <meta name="description" content="All characters of 'Rick and Morty' TV series" />
-        <link rel="icon" href="/favicon.ico" />
-      </Helmet>
+      <HelmetProvider>
+        <Helmet>
+          <title>Rick and Morty - All characters</title>
+          <meta name="description" content="All characters of 'Rick and Morty' TV series" />
+          <link rel="icon" href="/favicon.ico" />
+        </Helmet>
+      </HelmetProvider>
       <Header>
       </Header>
       <Content className={styles.content} >
